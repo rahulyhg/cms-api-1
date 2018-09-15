3ee2d4b394cf3cb155f637dc8d3a9005ee5b88a5
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2018-09-15 20:35:30 +0200

    Add zend framework skeleton

---
+++ source/config/modules.config.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'Zend\Form',
+    'Zend\Cache',
+    'Zend\Db',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\InputFilter',
+    'Zend\I18n',
+    'Zend\Log',
+    'Zend\Mvc\I18n',
+    'Zend\Paginator',
+    'Zend\Router',
+    'Zend\Validator',
+    'Zend\Router',
+    'Zend\Validator',
+
+    'ZF\Apigility',
+    'ZF\ApiProblem',
+    'ZF\MvcAuth',
+    'ZF\OAuth2',
+    'ZF\Hal',
+    'ZF\ContentNegotiation',
+    'ZF\ContentValidation',
+    'ZF\Rest',
+    'ZF\Rpc',
+    'ZF\Versioning',
+    'ZF\Apigility\Documentation',
+
+    'DoctrineModule',
+    'DoctrineORMModule',
+
+    'Application',
+];
