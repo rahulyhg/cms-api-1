31ddc9a75475af12f2d86daf29acfd3b89f7f569
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2018-09-16 11:18:41 +0200

    Optimize ZF Module create

--- source/zend/zf-module-creator.php
+++ source/zend/zf-module-creator.php
@@ -4,7 +4,15 @@ define('_BASE_', __DIR__.'/..');
 
 $command = $argv[1];
 
-function createTemplate($path, $module)
+/**
+ * Copy template of module into the actual module
+ *
+ * @param $path   string
+ * @param $module string
+ *
+ * @return void
+ */
+function createTemplate(string $path, string $module): void
 {
     $moduleDir = _BASE_.'/module/'.$module.$path.'/';
     $templateDir = __DIR__.'/template'.$path.'/';
@@ -31,19 +39,68 @@ function createTemplate($path, $module)
     $currentDirectory->close();
 }
 
+/**
+ * @param string $module
+ *
+ * @return void
+ */
+function updateNamespace(string $module): void
+{
+    $composer = _BASE_.'/composer.json';
+
+    $oldJson = json_decode(file_get_contents($composer), true);
+    unset($oldJson['autoload']['psr-4'][$module.'\\']);
+
+    file_put_contents(
+        $composer,
+        json_encode(array_merge_recursive(
+            ['autoload' => ['psr-4' => [$module.'\\' => 'module/'.$module.'/src/']]],
+            $oldJson
+        ), JSON_PRETTY_PRINT)
+    );
+}
+
+/**
+ * @param string $module
+ *
+ * @return void
+ */
+function updateApplicationModule(string $module): void
+{
+    $appModule = _BASE_.'/config/modules.config.php';
+
+    if (in_array($module, include($appModule))) {
+        return;
+    }
+
+    $config = file_get_contents($appModule);
+    file_put_contents(
+        $appModule,
+        substr_replace(
+            $config,
+            "\t'{$module}',\n",
+            strpos($config, "];"),
+            0
+        )
+    );
+}
+
 switch ($command) {
     case "create":
         if (empty($argv[2])) {
-                die("Module name is empty\n");
+            die("Module name is empty\n");
         }
 
         $module = ucfirst($argv[2]);
         system("rm -rf "._BASE_.'/module/'.$module.'/');
 
         createTemplate('', $module);
+        updateNamespace($module);
+        updateApplicationModule($module);
+        system('composer dump-autoload');
         echo "End.\n";
         break;
     default:
-        echo "Welcome toMudule creator\n";
+        echo "Welcome to ZF2 Module Creator\n";
         break;
 }
