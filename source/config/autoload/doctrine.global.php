66462fda5706a6e77cfc0b82f8ff3d2d76e162da
Shahrokh Nabavi <shahrokhnabavi@yahoo.com>
2018-09-20 22:26:23 +0200

    add charset utf-8 to doctrine module

--- source/config/autoload/doctrine.global.php
+++ source/config/autoload/doctrine.global.php
@@ -1,6 +1,7 @@
 <?php
 
 use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 
 return [
     'doctrine' => [
@@ -10,6 +11,11 @@ return [
                 'params' => [
                     'host' => 'db',
                     'dbname' => 'web_db',
+                    'port' => '3306',
+                    'charset' => 'utf8',
+                    'driverOptions' => [
+                        1002 => 'SET NAMES utf8',
+                    ],
                 ],
             ],
         ],
@@ -20,6 +26,20 @@ return [
                 'name' => 'Doctrine Database Migrations',
                 'namespace' => 'Migrations',
                 'table' => 'migrations',
+            ],
+        ],
+        'driver' => [
+            'my_annotation_driver' => [
+                'class' => AnnotationDriver::class,
+                'cache' => 'array',
+                'paths' => [
+                    __DIR__.'../../module/Application/Entity',
+                ],
+            ],
+            'orm_default' => [
+                'drivers' => [
+                    'Application' => 'my_annotation_driver',
+                ],
             ],
         ],
     ],
